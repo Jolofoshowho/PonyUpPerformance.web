c2f80b875434b53f67d9974a6a602435dc8cd6ba
Jolofoshowho <lopezkb258@gmail.com>
2026-06-28 11:04:27 -0700

    Add PricingPageModel for Pricing page

---
+++ PonyUpPerformance.Web/Pages/Pricing.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PonyUpPerformance.Web.Pages
+{
+    public class PricingPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
