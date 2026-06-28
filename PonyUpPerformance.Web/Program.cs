0371646f64229ca92ce92b5f0902b2b1194f3f3d
Jolofoshowho <lopezkb258@gmail.com>
2026-06-28 11:20:57 -0700

    Set Stripe API key from configuration

--- PonyUpPerformance.Web/Program.cs
+++ PonyUpPerformance.Web/Program.cs
@@ -7,6 +7,9 @@ using PonyUpPerformance.Web.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Stripe.StripeConfiguration.ApiKey =
+    builder.Configuration["Stripe:SecretKey"];
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
