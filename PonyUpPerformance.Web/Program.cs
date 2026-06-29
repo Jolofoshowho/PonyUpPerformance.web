5bcc60dc225449c33679ee60e04272ef5fd1b445
Jolofoshowho <lopezkb258@gmail.com>
2026-06-29 10:27:48 -0700

    Remove database deletion in Program.cs

--- PonyUpPerformance.Web/Program.cs
+++ PonyUpPerformance.Web/Program.cs
@@ -45,7 +45,6 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-    db.Database.EnsureDeleted();
     db.Database.EnsureCreated();
 }
 
