0b274aac631f03ae43ac7ed7b12d8dca8630ca6c
Jolofoshowho <lopezkb258@gmail.com>
2026-07-19 00:36:49 -0700

    Add properties to DecisionResult class

--- PonyUpPerformance.Web/Models/DecisionResult.cs
+++ PonyUpPerformance.Web/Models/DecisionResult.cs
@@ -1,11 +1,17 @@
-﻿namespace PonyUpPerformance.Web.Models;
+namespace PonyUpPerformance.Web.Models;
 
 public class DecisionResult
 {
+    public int Score { get; set; }
+
     public string Recommendation { get; set; } = string.Empty;
+
     public int ConfidenceScore { get; set; }
+
     public string RiskLevel { get; set; } = string.Empty;
+
     public string FinancialImpact { get; set; } = string.Empty;
+
     public string Reasoning { get; set; } = string.Empty;
 
     public List<string> NextSteps { get; set; } = new();
