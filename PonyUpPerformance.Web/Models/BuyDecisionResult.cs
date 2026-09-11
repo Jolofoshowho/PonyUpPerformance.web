f35a9a1940f71e8ba05917b723264d3c2da21998
Jolofoshowho <lopezkb258@gmail.com>
2026-09-11 05:01:35 -0700

    Update BuyDecisionResult.cs

--- PonyUpPerformance.Web/Models/BuyDecisionResult.cs
+++ PonyUpPerformance.Web/Models/BuyDecisionResult.cs
@@ -2,9 +2,13 @@ namespace PonyUpPerformance.Web.Models;
 
 public class BuyDecisionResult : DecisionResult
 {
-    public decimal MaximumRecommendedPrice { get; set; }
+    public decimal? FairAskingPrice { get; set; }
 
-    public decimal FairPurchasePrice { get; set; }
+    public decimal? MaximumRecommendedPrice { get; set; }
 
-    public decimal SuggestedFirstOffer { get; set; }
+    public decimal? FairPurchasePrice { get; set; }
+
+    public decimal? SuggestedFirstOffer { get; set; }
+
+    public decimal? BuyerNegotiationMarginPercent { get; set; }
 }
