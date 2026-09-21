36fb15d0537e813572e67b3101bac282a79e3d8d
Jolofoshowho <lopezkb258@gmail.com>
2026-09-21 02:18:21 -0700

    Remove duplicate registration of ITradeScoringService

--- PonyUpPerformance.Web/Program.cs
+++ PonyUpPerformance.Web/Program.cs
@@ -88,6 +88,7 @@ builder.Services.AddScoped<UsageCreditService>();
 builder.Services.AddScoped<IBuyScoringService, BuyScoringService>();
 builder.Services.AddScoped<ISellScoringService, SellScoringService>();
 builder.Services.AddScoped<ITradeScoringService, TradeScoringService>();
+builder.Services.AddScoped<ITradeScoringService, TradeScoringService>();
 
 var app = builder.Build();
 
